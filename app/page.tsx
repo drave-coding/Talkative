f2a7fc22bf1e3f1444b6fc372956edaccc9cf346
Abhishek Das <abhishek2022.work@gmail.com>
2025-06-05 13:46:01 +0530

    deployment fix in page.tsx for static loads

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import {
 import { getSubjectColor } from "@/lib/utils";
 import React from "react";
 
+export const dynamic = "force-dynamic";
 const Page = async () => {
   const companions = await getAllCompanions({ limit: 3 });
   const recentSessionsCompanions = await getRecentSessions(5);
